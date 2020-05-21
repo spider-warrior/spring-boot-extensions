f532948983ced55c515870b002c377528e09fbbc
yangjian <yangjian@ishansong.com>
2020-05-21 21:36:32 +0800

    LogbackUtil完善

---
+++ spring-boot-extension-starters/trace-extension-spring-boot-starter/src/main/java/cn/t/extension/springboot/starters/trace/LogbackUtil.java
@@ -0,0 +1,45 @@
+package cn.t.extension.springboot.starters.trace;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy;
+import ch.qos.logback.core.util.FileSize;
+import cn.t.common.trace.loback.JsonLogLayout;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author yj
+ * @since 2020-05-21 12:42
+ **/
+public class LogbackUtil {
+
+    private static Logger buildFileAppenderLogger(String name, String fileName, int maxHistory) {
+        LoggerContext loggerContext = (LoggerContext)LoggerFactory.getILoggerFactory();
+        Logger logger = loggerContext.getLogger(name);
+        logger.setAdditive(false);
+        logger.addAppender(buildRollingFileAppender(loggerContext, fileName, maxHistory));
+        return logger;
+    }
+
+    private static RollingFileAppender<ILoggingEvent> buildRollingFileAppender(LoggerContext loggerContext, String fileName, int maxHistory) {
+        RollingFileAppender<ILoggingEvent> appender = new RollingFileAppender<>();
+        appender.setName("TracedRollingFileAppender");
+
+        //rolling policy
+        SizeAndTimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new SizeAndTimeBasedRollingPolicy<>();
+        rollingPolicy.setContext(loggerContext);
+        rollingPolicy.setParent(appender);
+        rollingPolicy.setFileNamePattern(fileName + ".%d{yyyy-MM-dd}.%i.log");
+        rollingPolicy.setMaxHistory(maxHistory);
+        rollingPolicy.setMaxFileSize(FileSize.valueOf("100MB"));
+        rollingPolicy.start();
+
+        appender.setContext(loggerContext);
+        appender.setRollingPolicy(rollingPolicy);
+        appender.setLayout(new JsonLogLayout());
+        appender.start();
+        return appender;
+    }
+}
